969d6a74b337cbab910fe9df053742f4d80d0a32
ryujignh <ryujignh@gmail.com>
2022-02-16 15:15:36 -0800

    Add redirect controller

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -1,13 +1,8 @@
-class LinksController < ApplicationController
-
-  def index
-    @links = Link.all
-  end
+class RedirectController < ApplicationController
 
   def show
-    @link = Link.find_by_slug(params[:slug])
-    render 'error/404', status: 404 if @link.nil?
-    @link.update_attribute(:clicked, @link.clicked + 1)
-    redirect_to @link.url, allow_other_host: true
+    url = params[:url]
+    render 'error/404', status: 404 if url.nil?
+    redirect_to url, allow_other_host: true
   end
 end
